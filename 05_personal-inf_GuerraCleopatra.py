1d9cd1755576e4886b6646f5f55767f9fe2568ac
cleo-guerra <87781922+cleo-guerra@users.noreply.github.com>
2021-08-07 18:29:01 -0500

    Add files via upload

---
+++ 05_personal-inf_GuerraCleopatra.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 18:41:37 2021
+
+@author: DELL
+"""
+#variables
+
+nombre = input ("Ingresa tu nombre: ")
+apellido = input ("Ingresa tu apellido: ")
+localizacion = input ("Ingresa tu localizacion: ")
+edad = int(input("Ingresa tu edad: "))
+space = ' '
+print("Hola",space, nombre, space, apellido, space, localizacion, space, edad)
+#Otra forma
+#print("Hola"+space+ nombre + space + apellido + space + localizacion + space + edad)
